d8b3ac834ef0beb3025540065e5cb408f06362d9
yasmin.amalia <yasmin.amalia@ui.ac.id>
2019-10-14 08:51:17 +0700

    feat: initialize spring boot project

---
+++ sidok/src/main/java/apap/tugas/sidok/SidokApplication.java
@@ -0,0 +1,13 @@
+package apap.tugas.sidok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidokApplication.class, args);
+	}
+
+}
